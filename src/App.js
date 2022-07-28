f4d4619f4eca14978dc61b6e8b57c5f427056137
ThanhTai <thanhtaiho188@gmail.com>
2022-07-29 00:30:53 +0700

    Two-Way-Binding

--- src/App.js
+++ src/App.js
@@ -2,31 +2,143 @@ import { useState } from 'react';
 import './App.css';
 
 
-const gifts = [
-  'Gift1',
-  'Gift2',
-  'Gift3'
-]
 
-function App() {
+//============TWO-WAY-BINDING in FORM===================
+// function App() {
 
-  const [gift,setGift] = useState()
+//   const [name ,setName] = useState('')
+//   const [email ,setEmail] = useState('')
 
-  const randomGift = () => {
-    const index = Math.floor(Math.random() * gifts.length)
+//   const handelSubmit = () =>{
+//     console.log({
+//       name,
+//       email
+//     })
 
-    setGift(gifts[index])
+//   }
 
+//   return (
+//     <div className="App">
+//       <input 
+//         value={name}
+//         onChange = {e => setName(e.target.value)}
+
+//       />
+//       <input 
+//         value={email}
+//         onChange = {e => setEmail(e.target.value)}
+
+//       />
+//       <button onClick={handelSubmit}>Register</button>
+//     </div>
+//   );
+// }
+
+
+
+
+//TWO-WAY-BINDING in RADIO==========================
+
+// const courses = [
+//   {
+//     id:1,
+//     name: "HTML,CSS"
+//   },
+//   {
+//     id:2,
+//     name: "Javascript"
+//   },
+//   {
+//     id:3,
+//     name: "ReactJS"
+//   }
+// ]
+// function App() {
+//   const [checked, setChecked] = useState(1)
+
+
+//   const handelSubmit = () =>{
+//     console.log({id: checked})
+
+//     }
+
+
+//   return (
+//     <div className="App">
+//       {
+//         courses.map(course => (
+//           <div key={course.id}>
+//             <input 
+//               checked={ checked === course.id}
+//               type="radio"
+//               onChange={() => setChecked(course.id)}
+//               />
+//               {course.name}
+//           </div>
+//         ))}
+//       <button onClick={handelSubmit}>Register</button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+//========================TWO-WAY-BINDING in CHECKBOX==================
+
+const courses = [
+  {
+    id:1,
+    name: "HTML,CSS"
+  },
+  {
+    id:2,
+    name: "Javascript"
+  },
+  {
+    id:3,
+    name: "ReactJS"
   }
+]
+function App() {
+  const [checked, setChecked] = useState([])
 
- 
+
+  console.log(checked)
+
+  const handleCheck = (id) => {
+    setChecked(prev =>{
+      const isChecked =  checked.includes(id)
+
+      if(isChecked){
+        return checked.filter(item => item !== id)
+      }else{
+        return [...prev,id]
+      }
+    })
+  }
+  const handelSubmit = () =>{
+    console.log({ids:checked})
+
+    }
+
 
   return (
     <div className="App">
-      <h1>{gift || 'Chua co phan thuong'}</h1>
-      <button onClick={randomGift}>Lay thuong</button>
+      {
+        courses.map(course => (
+          <div key={course.id}>
+            <input 
+              checked={ checked.includes(course.id)}
+              type="checkbox"
+              onChange={() => handleCheck(course.id)}
+              />
+              {course.name}
+          </div>
+        ))}
+      <button onClick={handelSubmit}>Register</button>
     </div>
-  );
+  )
 }
 
 export default App;
